0403a899423f7d4886171915827ec9ed465f0a8a
Daniel Walter <danielwalterpand@gmail.com>
2017-10-03 20:39:45 -0300

    Introdução

--- src/BalancoTrimestral.java
+++ src/BalancoTrimestral.java
@@ -1,12 +1,14 @@
 
 public class BalancoTrimestral {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarço = 17000;
+		int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarço;
+		
+		System.out.println(gastosTrimestre);
 	}
 
 }
